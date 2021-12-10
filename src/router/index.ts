2aeb84ea4388bf5e132b4085b52d455a038f0200
hujinbin <437044048@qq.com>
2021-12-10 17:59:02 +0800

    feat: 增加全局布局容器

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,9 @@
 
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+// 基本布局
+import layout from '../layout/index.vue';
+
 // 1. 定义路由组件.
 import HelloWorld from '../page/HelloWorld.vue';
 // 也可以从其他文件导入
@@ -12,9 +15,16 @@ const About = { template: '<div>About</div>' }
 // 每个路由都需要映射到一个组件。
 // 我们后面再讨论嵌套路由。
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/hello', component: HelloWorld },
+  {
+    path:'/',
+    component:layout,
+    children:[
+      { path: '/', component: Home },
+      { path: '/about', component: About },
+      { path: '/hello', component: HelloWorld },
+    ],
+  },
+  
 ]
 
 // 3. 创建路由实例并传递 `routes` 配置
