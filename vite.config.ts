a8b138c834a365ea18c8399976fceb5539b8d7ad
hujinbin <437044048@qq.com>
2021-12-11 17:00:31 +0800

    build: vite-plugin-eslint和相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+import eslintPlugin from 'vite-plugin-eslint'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  },
+  }
 })
